bb5bf428a04b4da7d1213db5960b0289beb3380a
jai1 <jai1@users.noreply.github.com>
2017-07-06 15:05:02 -0700

    Update smartPointers.cpp

--- smartPointers.cpp
+++ smartPointers.cpp
@@ -6,7 +6,7 @@
                - this leads to A and B never deleted
                - use weak pointer or references to avoid this   
     weak_ptr - need to call lock() to get the data
-             - lock upgrades the weak_ptr to shared_ptr if object is not deleted, else exception. In order to avoid exception check for isExpired.
+             - lock upgrades the weak_ptr to shared_ptr if object is not deleted, else .lock() operation gives null (use isExpired api)
 
 */
 #include<iostream>
