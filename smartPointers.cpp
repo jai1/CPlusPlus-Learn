33baf97904f6261eec6f49613039bd06706222c3
jai1 <jai1@users.noreply.github.com>
2017-07-06 15:03:23 -0700

    Update smartPointers.cpp

--- smartPointers.cpp
+++ smartPointers.cpp
@@ -9,3 +9,31 @@
              - lock upgrades the weak_ptr to shared_ptr if object is not deleted, else exception. In order to avoid exception check for isExpired.
 
 */
+#include<iostream>
+#include<boost/shared_ptr.hpp>
+#include<boost/make_shared.hpp>
+#include<boost/weak_ptr.hpp>
+using namespace std;
+
+int main() {
+	// Program 1
+	boost::shared_ptr<int> sharedPtr = boost::make_shared<int>(10);
+	boost::weak_ptr<int> weakPtr = sharedPtr;
+	cout<<*weakPtr.lock()<<endl; // prints 10 since weak_ptr points to sharedPtr
+
+	boost::shared_ptr<int> sharedPtr2 = weakPtr.lock();
+	sharedPtr2.reset();
+	cout<<weakPtr.lock()<<endl; // weakPtr is still valid since sharedPtr is not destroyed
+
+	weakPtr.reset();
+	cout<<weakPtr.lock()<<endl; // Now weakPtr points to null
+
+
+	// Program 2
+	sharedPtr2 = sharedPtr;
+	weakPtr = sharedPtr2;
+	sharedPtr2.reset();
+	cout<<weakPtr.lock()<<endl; // weakPtr is still valid since sharedPtr is not destroyed
+	return 0;
+}
+
